1f9a5d81b4629f52ad2bfdd2229f1c913aed5300
JALINDAR01 <134129652+JALINDAR01@users.noreply.github.com>
2023-09-12 21:58:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
+ 
+function compare(a, b) {
+  if (a < b) {
+      return -1;
+  } else if (a > b) {
+      return 1;
+  } else {
+      return 0;
+  }
+}
+ 
+ 
+ 
 function calculateMinCost() {
-  const input = document.getElementById("input").value;
-   const arr=input.split(",");
-
-	const heap = new array();
-	for (let i = 0; i < arr.length; i++) {
-		heap.push(Number(arr[i]));
-	}
-  let cost = 0;
-	while (heap.length>1) {
-		const first=heap.pop();
-		const second=heap.pop();
-
-		cost+=first+second;
-
-		heap.push(first+second);
-	}
+  //your code here
+  let str=document.getElementById('rope-lengths').value.split(",");
+  let arr=str.map((str)=>parseInt(str));
+  // console.log(arr);
   
-  return cost;
+let total=0;
+arr=arr.sort(compare);
+// console.log("arr ",arr);
+while(arr.length>=2){
+  let sum=arr[0]+arr[1];
+  // console.log(sum);
+  let rem=[];
+  rem.push(sum);
+  for(let k=2;k<arr.length;k++){
+    rem.push(arr[k]);
+  }
+  rem=rem.sort(compare);
+  // console.log("rem ",rem);
+  arr=[...rem];
+  // console.log("arr ",arr);
+  total=total+sum;
+  // console.log("total "+total+"----------------------");
+}
+console.log(total);
+ 
+ 
+let result=document.getElementById('result');
+result.innerHTML=total;
 }  
