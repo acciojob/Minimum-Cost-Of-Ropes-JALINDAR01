148c46e7c7100349dbdb59a21478ff5c19e51a19
JALINDAR01 <134129652+JALINDAR01@users.noreply.github.com>
2023-09-12 21:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function calculateMinCost() {
-  //your code here
+  const input = document.getElementById("input").value;
+   const arr=input.split(",");
+
+	const heap = new array();
+	for (let i = 0; i < arr.length; i++) {
+		heap.push(Number(arr[i]));
+	}
+  let cost = 0;
+	while (heap.length>1) {
+		const first=heap.pop();
+		const second=heap.pop();
+
+		cost+=first+second;
+
+		heap.push(first+second);
+	}
   
-  
-  
+  return cost;
 }  
